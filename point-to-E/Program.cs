2a05ec87a13b4800dabf9ee0b2869059c44b6cd6
MavProxyUser <hostile@me.com>
2019-08-15 23:16:40 -0400

    Add point-to-E example to launch e:\game\game.exe

---
+++ point-to-E/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace point_to_E
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.Sleep(2000);
+            System.Diagnostics.Process.Start(@"E:\Game\game.exe");
+        }
+    }
+}
